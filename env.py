9693d5daf43cfd81ba643a30a293d3207d4c0926
matheusbruno97 <128164927+matheusbruno97@users.noreply.github.com>
2024-01-12 21:49:00 -0300

    Create env.py

---
+++ env.py
@@ -0,0 +1,7 @@
+mlx_email_address = "your-mlx-address"
+mlx_password = "your-mlx-password"
+folder_id = "your-chosen-folder-id"
+profile_id = "your-chosen-profile-id"
+browser_type = "mimic" # for the time being, only Mimic. I'll add a function to instantiate a Stealthfox (Firefox) browser with the adequate capabilities (options=Options())
+trakt_username = "trakt-user"
+trakt_password = "trakt-password"
